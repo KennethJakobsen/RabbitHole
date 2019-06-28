c74372b48dbbf19729772dfe3676a6eb8954eb57
Kenneth Jakobsen <Kenneth.Jakobsen@yahoo.com>
2019-06-28 06:32:48 +0200

    added filtered list

---
+++ RabbitHole/Collections/RegexFilterList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace RabbitHole.Collections
+{
+    public class RegexFilterList : IEnumerable<string>
+    {
+        private readonly List<string> _list;
+        private readonly string filter;
+
+        public RegexFilterList(string filter, string wordlist)
+        {
+            var regex = new Regex(filter);
+            var matches = regex.Matches(wordlist);
+            
+            this.filter = filter;
+            
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
---
+++ RabbitHole/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using RabbitHole.Collections;
+
+namespace RabbitHole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var list = new RegexFilterList("^[pultryouwians]{1,18}$", File.ReadAllText("wordlist"));
+        }
+    }
+}
